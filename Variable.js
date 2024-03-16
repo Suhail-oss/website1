bfe3a9cf7a7d5ac916e442db46caf56a99ae08e2
Suhail-oss <135872832+Suhail-oss@users.noreply.github.com>
2024-03-16 12:46:13 +0530

    update

---
+++ Variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail= "suhail@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
